278c7829caccdd96ceab2ee3da5fb724a361952a
Alexey Demyanov <deal110988@him-mail.ch>
2023-07-31 21:12:03 +0200

    adding addresses

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ import { auth } from './auth.js';
 
 async function run() {
   let browser;
+  let page;
+  let body;
+  let html;
 
   try {
     browser = await puppeteer.connect({
       browserWSEndpoint: `wss://${auth}@brd.superproxy.io:9222`,
     });
 
+    // main script
+
+    page = await browser.newPage();
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+    await page.goto('https://amazon.com/');
+
+    body = await page.$('body');
+
+    html = await page.evaluate(() => {
+      document.documentElement.outerHTML;
+    });
+
+    console.log(html);
+
+    // main script end
+
     return;
   } catch (e) {
     console.error('scrape failed', e);
   } finally {
-    console.log(browser)
+    console.log(body);
+    console.log(html);
     await browser?.close();
   }
 }
